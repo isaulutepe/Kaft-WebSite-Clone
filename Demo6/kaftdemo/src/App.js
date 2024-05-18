02302ca36784cfdb428d69e33ec750bcc6ca0114
engingul <egul1987@gmail.com>
2024-05-18 14:59:59 +0300

    SplashScreen ekranı eklendi

--- Demo6/kaftdemo/src/App.js
+++ Demo6/kaftdemo/src/App.js
@@ -1,14 +1,31 @@
+// src/App.js
 
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './components/Router';
-
+import SplashScreen from './components/SplashScreen';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [isFading, setIsFading] = useState(false);
+
+  useEffect(() => {
+    // Simulating a loading delay
+    const timer = setTimeout(() => {
+      setIsFading(true); // Fade out başlat
+      setTimeout(() => {
+        setLoading(false); // Loading durumunu false yap
+      }, 1000); // Fade out süresi ile aynı olmalı
+    }, 3000); // 3 saniye sonra fade out başlat
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Router>
       <div className="App">
-        <Routes /> 
+        {loading ? <SplashScreen isFading={isFading} /> : <Routes />}
       </div>
     </Router>
   );
