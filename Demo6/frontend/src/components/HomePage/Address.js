34bd8ade3a8d2f407c2449f0d3e06060dd054a56
Senayldz <senasenayildiz@gmail.com>
2024-06-19 00:14:38 +0300

    Update Address.css

--- Demo6/frontend/src/components/HomePage/Address.js
+++ Demo6/frontend/src/components/HomePage/Address.js
@@ -21,56 +21,58 @@ function AddressForm() {
   const [country, setCountry] = useState('');
 
   return (
-    <div className="box">
-      <AddressField
-        name="Adres"
-        type="text"
-        placeholder="eg. Menteşe/Muğla"
-        className="small-input"
-      />
-      <AddressField
-        name="Posta kodu"
-        type="number"
-        placeholder=""
-        className="small-input"
-      />
-      <div className="field">
-        <label>Şehir:</label>
-        <div className="control">
-          <select
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            className="large-select"
-          >
-            <option value="">Şehir Seçin</option>
-            <option value="istanbul">İstanbul</option>
-            <option value="ankara">Ankara</option>
-            <option value="izmir">İzmir</option>
-            {/* Diğer şehirler */}
-          </select>
+    <div className="form-container"> {/* Yeni eklenen container */}
+      <div className="box">
+        <AddressField
+          name="Adres"
+          type="text"
+          placeholder="eg. Menteşe/Muğla"
+          className="small-input"
+        />
+        <AddressField
+          name="Posta kodu"
+          type="number"
+          placeholder=""
+          className="small-input"
+        />
+        <div className="field">
+          <label>Şehir:</label>
+          <div className="control">
+            <select
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              className="large-select"
+            >
+              <option value="">Şehir Seçin</option>
+              <option value="istanbul">İstanbul</option>
+              <option value="ankara">Ankara</option>
+              <option value="izmir">İzmir</option>
+              {/* Diğer şehirler */}
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="field">
-        <label>Ülke:</label>
-        <div className="control">
-          <select
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
-            className="large-select"
-          >
-            <option value="">Ülke Seçin</option>
-            <option value="turkey">Türkiye</option>
-            <option value="usa">Amerika Birleşik Devletleri</option>
-            <option value="uk">Birleşik Krallık</option>
-            {/* Diğer ülkeler */}
-          </select>
+        <div className="field">
+          <label>Ülke:</label>
+          <div className="control">
+            <select
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              className="large-select"
+            >
+              <option value="">Ülke Seçin</option>
+              <option value="turkey">Türkiye</option>
+              <option value="usa">Amerika Birleşik Devletleri</option>
+              <option value="uk">Birleşik Krallık</option>
+              {/* Diğer ülkeler */}
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link">Ekle</button>
-          <button className="button is-link">Güncelle</button>
-          <button className="button is-link">Sil</button>
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link">Ekle</button>
+            <button className="button is-link">Güncelle</button>
+            <button className="button is-link">Sil</button>
+          </div>
         </div>
       </div>
     </div>
